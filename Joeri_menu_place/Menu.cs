3034aa68bd0801097911dc6e157dd760f5ae822e
JoeriDekker <joeridekker2002@gmail.com>
2023-03-27 22:29:46 +0200

    Added a Remove dish function and a change Dish function
    Still need to make some exeption handlings for menu chooses

--- Joeri_menu_place/Menu.cs
+++ Joeri_menu_place/Menu.cs
@@ -41,20 +41,84 @@ public class Menu : IChangeable
         }
         Dish new_dish = new Dish(dish_name, dish_description, dish_price);
         _dishes.Add(new_dish);
+        SaveMenu();
     }
 
     public void Delete()
+{
+    Console.WriteLine("Which Dish do you want to remove? (Give the name of the dish)");
+    string remove_dish = Console.ReadLine();
+    bool found_dish = false;
+    List<Dish> dishesToRemove = new List<Dish>();
+    foreach (Dish dish in _dishes)
     {
-        // ask for dish name
-        // loop dishes and check if dish name match
-            // _dishes.Remove();
+        if (dish.Name == remove_dish)
+        {
+            dishesToRemove.Add(dish);
+            found_dish = true;
+        }
     }
+    if (!found_dish)
+    {
+        Console.WriteLine($"{remove_dish} has not been found");
+    }
+    else
+    {
+        foreach (Dish dish in dishesToRemove)
+        {
+            _dishes.Remove(dish);
+        }
+        SaveMenu();
+        Console.WriteLine($"{remove_dish} has been removed from the menu");
+    }
+}
+
 
     public void Update()
     {
-        // dishToChange.Name = newName;
-        // dishToChange.Description = newDescription;
-        // dishToChange.Price = newPrice;
+        Console.WriteLine("Which Dish do you want to Change? (Give the name of the dish)");
+        string? change_dish = Console.ReadLine();
+        bool found_dish = false;
+        foreach (Dish dish in _dishes){
+            if (dish.Name == change_dish){
+                while (true){
+                    Console.WriteLine("What do you want to change?");
+                    Console.WriteLine("1: Change Name");
+                    Console.WriteLine("2: Change Description");
+                    Console.WriteLine("3: Change Price");
+                    Console.WriteLine("4: Save Changes");
+                    int choosed_number = Convert.ToInt32(Console.ReadLine());
+                    if (choosed_number == 1){
+                        Console.WriteLine("What is the new dish name?");
+                        string? new_dish_name = Console.ReadLine();
+                        dish.Name = new_dish_name;
+                    }
+                    else if (choosed_number == 2){
+                        Console.WriteLine("What is the new dish description?");
+                        string? new_dish_description = Console.ReadLine();
+                        dish.Description = new_dish_description;
+                    }
+                    else if (choosed_number == 3){
+                         Console.WriteLine("What is the new dish description?");
+                        double new_dish_price = Convert.ToDouble(Console.ReadLine());
+                        dish.Price = new_dish_price;
+                    }
+                    else if (choosed_number == 4){
+                            break;
+                    }
+                    else{
+                        Console.WriteLine("Input error! Please enter an number");
+                    }
+                }
+            }
+        }
+        if (!found_dish){
+            Console.WriteLine($"{change_dish} has not been found");
+        }
+        else{
+            SaveMenu();
+            Console.WriteLine($"{change_dish} has been updated");
+        }
     }
 
     public void SaveMenu()
--- Program.cs
+++ Program.cs
@@ -4,12 +4,45 @@ UI.Start();
 
 
 // Joeri his menu test section
-    // Menu menu = new Menu();
+//    Menu menu = new Menu();
+//         menu.LoadMenu();
 
-    // // Add a new dish to the menu
-    // menu.LoadMenu();
-    // menu.Add();
+//         while (true)
+//         {
+//             Console.WriteLine("What do you want to do?");
+//             Console.WriteLine("1: Show Menu");
+//             Console.WriteLine("2: Add a Dish");
+//             Console.WriteLine("3: Remove a Dish");
+//             Console.WriteLine("4: Change a Dish");
+//             Console.WriteLine("5: Save Menu");
+//             Console.WriteLine("6: Exit");
 
-    // Console.WriteLine("Current Menu:");
-    // menu.ShowMenu();
-    // menu.SaveMenu();
+//             int choice = Convert.ToInt32(Console.ReadLine());
+
+//             switch (choice)
+//             {
+//                 case 1:
+//                     menu.ShowMenu();
+//                     break;
+//                 case 2:
+//                     menu.Add();
+//                     break;
+//                 case 3:
+//                     menu.Delete();
+//                     break;
+//                 case 4:
+//                     menu.Update();
+//                     break;
+//                 case 5:
+//                     menu.SaveMenu();
+//                     break;
+//                 case 6:
+//                     Console.WriteLine("Goodbye!");
+//                     return;
+//                 default:
+//                     Console.WriteLine("Invalid choice.");
+//                     break;
+//             }
+
+//             Console.WriteLine();
+//         }
