cd7635249e25e4bb79ae261af09099e383fae16c
JoeriDekker <joeridekker2002@gmail.com>
2023-03-27 16:00:02 +0200

    enabled UI.start

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to this amazing program");
-// UI.Start();
+UI.Start();
 
 
 // Joeri his menu test section
