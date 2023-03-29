d0169a643d3df81a245cd2e22e2f4266ae750073
Boy-Stroo <1061579@hr.nl>
2023-03-29 13:44:35 +0200

    Ingredient class added

---
+++ Joeri_menu_place/Ingredient.cs
@@ -0,0 +1,13 @@
+class Ingredient
+{
+    public string Name { get; set; }
+    public int Quantity { get; set;}
+    public string Unit { get; set; }
+
+    public Ingredient(string name, int quantity, string unit)
+    {
+        Name = name;
+        Quantity = quantity;
+        Unit = unit;
+    }
+}
