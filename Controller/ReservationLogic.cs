87cd1b33fdbc4f6b44a6d94653ffb652fbe3905a
Cyberwizard-v <veke2003@Outlook.com>
2023-03-30 17:16:14 +0200

    WIP | Show all reservations

--- Controller/ReservationLogic.cs
+++ Controller/ReservationLogic.cs
@@ -29,10 +29,20 @@ public class ReservationLogic{
 
     }
 
-    public ReservationModel GetById(int id)
+    public void GetAllReservations()
     {
-        return _Reservations.Find(i => i.R_Id == id);
+                    Console.WriteLine(@$"
+  ____                                _   _                 
+  |  _ \ ___  ___  ___ _ ____   ____ _| |_(_) ___  _ __  ___ 
+  | |_) / _ \/ __|/ _ \ '__\ \ / / _` | __| |/ _ \| '_ \/ __|
+  |  _ <  __/\__ \  __/ |   \ V / (_| | |_| | (_) | | | \__ \
+  |_| \_\___||___/\___|_|    \_/ \__,_|\__|_|\___/|_| |_|___/
+ =============================================================");
+    // Get all reservations || Create Table || String formatiing
+    Console.WriteLine("{0,-5} {1,-20} {2,-20} {3,-10} {4,-10}", "R_ID", "Contact", "R_time", "R_TableID", "P_Amount");
+    foreach (ReservationModel Res in _Reservations)
+    {
+        Console.WriteLine("{0,-5} {1,-20} {2,-20} {3,-10} {4,-10}", Res.R_Id, Res.Contact, Res.R_time, Res.R_TableID, Res.P_Amount);
     }
-
-
+}
 }
