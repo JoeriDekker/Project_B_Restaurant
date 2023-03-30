a056521bd09d9005d65b24136bf00e9e3fe2cc6e
Cyberwizard-v <veke2003@Outlook.com>
2023-03-30 16:44:16 +0200

    WIP | Create Reservation ( logic )

--- Controller/ReservationLogic.cs
+++ Controller/ReservationLogic.cs
@@ -1,3 +1,33 @@
-public class ReservationController{
-    
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class ReservationLogic{
+
+    private List<ReservationModel> _Reservations;
+
+    static public ReservationModel? CurrentReservation { get; private set; }
+
+    //Get all reservations
+    public ReservationLogic()
+    {
+        _Reservations = ReservationAccess.LoadAll();
+    }
+
+    public void CreateReservation(){
+        
+        // We need to create a reservation model
+        ReservationModel res = new ReservationModel(1, "Test","InsertTIME", 1, "million");
+        
+        //Add to daaaaaaaaaa list c:
+        _Reservations.Add(res);
+
+        // Save this data to Reservation.js
+        ReservationAccess.WriteAll(_Reservations);
+
+    }
+
+
 }
