3e166ad74c3693452caba3e46c75fe0f2eb7b8af
Cyberwizard-v <veke2003@Outlook.com>
2023-03-30 17:21:28 +0200

    WIP | Handle null in RAccess

--- Model/ReservationAccess.cs
+++ Model/ReservationAccess.cs
@@ -7,9 +7,22 @@ static class ReservationAccess
 
     public static List<ReservationModel> LoadAll()
     {
+        //If file does not exist in path
+        if (!File.Exists(path))
+        {
+            return new List<ReservationModel>();
+        }
+        
         string json = File.ReadAllText(path);
-        return JsonSerializer.Deserialize<List<ReservationModel>>(json);
+        //If json is null or empty ?
+        if (string.IsNullOrEmpty(json))
+        {
+            return new List<ReservationModel>();
+        }
 
+        //Null-coalescing operator | it can be either a List<ReservationModel> or null.
+        List<ReservationModel>? reservations = JsonSerializer.Deserialize<List<ReservationModel>>(json);
+        return reservations ?? new List<ReservationModel>();
     }
 
 
