e745513a8ac324e0da1b82210d7c5d0d890bb375
Cyberwizard-v <veke2003@Outlook.com>
2023-03-30 16:07:19 +0200

    WIP | Reservation model

--- Model/ReservationModel.cs
+++ Model/ReservationModel.cs
@@ -1,3 +1,28 @@
+using System.Text.Json.Serialization;
+
 public class ReservationModel{
     
+    [JsonPropertyName("R_id")]
+    public int R_Id { get; set; }
+
+    [JsonPropertyName("Contact")]
+    public string Contact { get; set; }
+
+    [JsonPropertyName("R_time")]
+    public string R_time { get; set; }
+
+    [JsonPropertyName("R_TableID")]
+    public int R_TableID { get; set; }
+
+    [JsonPropertyName("P_Amount")]
+    public string P_Amount { get; set; }
+
+    public ReservationModel(int R_id, string contactInfo, string r_time, int R_tableID, string P_amount)
+    {
+        this.R_Id = R_id;
+        this.Contact = contactInfo;
+        this.R_time = r_time;
+        this.R_TableID = R_tableID;
+        this.P_Amount = P_amount;
+    }
 }
