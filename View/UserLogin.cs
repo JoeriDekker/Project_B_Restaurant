6af026899924502ea86fee89aaa9bacd49a704d9
JosefineB04 <95631259+JosefineB04@users.noreply.github.com>
2023-04-01 02:18:49 +0200

    Reset password
    The user can now change their password.

--- View/UserLogin.cs
+++ View/UserLogin.cs
@@ -22,6 +22,43 @@ static class UserLogin
         else
         {
             Console.WriteLine("No account found with that email and password");
+            Console.WriteLine("Did you forget your password?");
+            string answer = "";
+            while (answer != "1" || answer != "2")
+            {
+                Console.WriteLine("Enter 1 to reset your password");
+                Console.WriteLine("Enter 2 to try log in again");
+                answer = Console.ReadLine();
+                if (answer == "1")
+                {
+                    ResetPassword();
+                }
+                else if (answer == "2")
+                {
+                    Start();
+                }
+            }
+        }
+    }
+    private static void ResetPassword()
+    {
+        Console.WriteLine("Please enter your email: ");
+        string email = Console.ReadLine();
+        AccountModel acc = accountsLogic.GetByEmail(email);
+        Console.WriteLine("Please enter your new password: ");
+        string new_password = Console.ReadLine();
+        Console.WriteLine("Please enter your new password again to confirm: ");
+        string confirm_password = Console.ReadLine();
+        if (new_password == confirm_password)
+        {
+            acc.Password = new_password;
+            accountsLogic.UpdateList(acc);
+            Console.WriteLine("Your password has been updated!");
+            UI.Start();
+        }
+        else
+        {
+            ResetPassword();
         }
     }
 }
