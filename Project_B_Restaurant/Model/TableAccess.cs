b5c29f97dfd9e6a3d3a7b015b95d7e5f7fecbcea
Cyberwizard-v <veke2003@Outlook.com>
2023-04-26 01:00:46 +0200

    WIP Tables | Init Access file

---
+++ Project_B_Restaurant/Model/TableAccess.cs
@@ -0,0 +1,35 @@
+using System.Text.Json;
+
+static class TableAccess
+{
+    static string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.CurrentDirectory, @"DataSources/Reservations.json"));
+
+
+    public static List<TableModel> LoadAll()
+    {
+        //If file does not exist in path
+        if (!File.Exists(path))
+        {
+            return new List<TableModel>();
+        }
+        
+        string json = File.ReadAllText(path);
+        //If json is null or empty ?
+        if (string.IsNullOrEmpty(json))
+        {
+            return new List<TableModel>();
+        }
+
+        //Null-coalescing operator it can be either a List<ReservationModel> or null.
+        List<TableModel>? reservations = JsonSerializer.Deserialize<List<TableModel>>(json);
+        return reservations ?? new List<TableModel>();
+    }
+
+
+    public static void WriteAll(List<TableModel> tables)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string json = JsonSerializer.Serialize(tables, options);
+        File.WriteAllText(path, json);
+    }
+}
