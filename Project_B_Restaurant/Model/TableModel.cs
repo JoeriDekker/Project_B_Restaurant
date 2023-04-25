cccf67f62b94ed5daf9e72003691d47785ce0933
Cyberwizard-v <veke2003@Outlook.com>
2023-04-26 00:59:50 +0200

    WIP Tables | Init TableModel

---
+++ Project_B_Restaurant/Model/TableModel.cs
@@ -0,0 +1,27 @@
+using System.Text.Json.Serialization;
+
+public class TableModel{
+    
+    [JsonPropertyName("T_ID")]
+    public string T_ID { get; set; }
+
+    [JsonPropertyName("Occupied")]
+    public bool Occupied { get; set; }
+
+    [JsonPropertyName("ReservedTime")]
+    public string ReservedTime { get; set; }
+        
+    [JsonPropertyName("T_Seats")]
+    public int T_Seats { get; set; }
+
+    [JsonPropertyName("R_Code")]
+    public string R_Code { get; set; }
+    public TableModel(string T_id, bool occupied, string reservedTime, int t_Seats, string r_Code)
+    {
+        this.T_ID = T_id;
+        this.Occupied = occupied;
+        this.ReservedTime = reservedTime;
+        this.T_Seats = t_Seats;
+        this.R_Code = r_Code;
+    }
+}
