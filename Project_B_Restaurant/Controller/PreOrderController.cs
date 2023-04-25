346fed3a6453551b7b0906f0476fa535d871da85
Charriexx <charlotte.e.kwok@gmail.com>
2023-04-25 15:23:47 +0200

    Pre Orders veranderd

--- Project_B_Restaurant/Controller/PreOrderController.cs
+++ Project_B_Restaurant/Controller/PreOrderController.cs
@@ -30,8 +30,75 @@ public class PreOrderController
         return null;
     }
 
-    public void PreOrderCourseMenu()
+    public Dish PreOrderAppetizer()
     {
-        
+        //pre order appetizer
+        Console.WriteLine("What appetizer do you want to order?");
+        foreach (Dish dish in _dishes)
+        {
+            if (dish.PreOrderAmount < dish.MaxAmountPreOrder && dish.Type == "Appetizer")
+            {
+                Console.WriteLine($"- {dish.Name}\n{string.Join(", ",dish.Ingredients)}\n{dish.Price}");
+            }
+        }
+        Console.WriteLine("Please type here which appetizer you want to pre order:\n");
+        string? preOrderAppetizer = Console.ReadLine();
+        foreach (Dish dish in _dishes)
+        {
+            if (dish.Name == preOrderAppetizer)
+            {
+                dish.PreOrderAmount++;
+                return dish;
+            }
+        }
+        return null;
+    }
+
+    public Dish PreOrderMain()
+    {
+        //pre order main
+        Console.WriteLine("\nWhat main dish do you want to order?");
+        foreach (Dish dish in _dishes)
+        {
+            if (dish.PreOrderAmount < dish.MaxAmountPreOrder && dish.Type == "Main")
+            {
+                Console.WriteLine($"- {dish.Name}\n{string.Join(", ",dish.Ingredients)}\n{dish.Price}");
+            }
+        }
+        Console.WriteLine("Please type here which main you want to pre order:\n");
+        string? preOrderMain = Console.ReadLine();
+        foreach (Dish dish in _dishes)
+        {
+            if (dish.Name == preOrderMain)
+            {
+                dish.PreOrderAmount++;
+                return dish;
+            }
+        }
+        return null;
+    }
+
+    public Dish preOrderDessert()
+    {
+        //pre order dessert
+        Console.WriteLine("\nWhat dessert do you want to order?");
+        foreach (Dish dish in _dishes)
+        {
+            if (dish.PreOrderAmount < dish.MaxAmountPreOrder && dish.Type == "Dessert")
+            {
+                Console.WriteLine($"- {dish.Name}\n{string.Join(", ",dish.Ingredients)}\n{dish.Price}");
+            } 
+        }
+        Console.WriteLine("Please type here which dessert you want to pre order:\n");
+        string? preOrderDessert = Console.ReadLine();
+        foreach (Dish dish in _dishes)
+        {
+            if (dish.Name == preOrderDessert)
+            {
+                dish.PreOrderAmount++;
+                return dish;
+            }
+        }
+    return null;
     }
 }
